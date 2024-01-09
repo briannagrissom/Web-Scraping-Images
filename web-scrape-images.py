e06f5bd5674c0bca9a7249395e7f07f44f1d585c
Brianna Grissom <114755509+briannagrissom@users.noreply.github.com>
2024-01-09 13:16:01 -0800

    Update and rename code to web-scrape-images.py

---
+++ web-scrape-images.py
@@ -0,0 +1,25 @@
+import requests
+from scrapy import Selector
+import urllib.request
+
+# Obtain HTML
+html= requests.get('https://all-free-download.com/free-photos/crocodile.html').text
+
+# Initiate selector
+selector = Selector(text=html)
+
+# Obtain URLs for the images
+URLs = selector.css('img::attr(src)').extract()
+
+# Filter through the URLs to use URLs of images only
+images = []
+for item in URLs:
+    if '.jpg' in item:
+        images.append(item) 
+
+# Write each image to a specific directory
+number=0
+for item in images:
+    path= '/Users/briannagrissom/CrocGator/croc'
+    urllib.request.urlretrieve(item, path + str(number) +'.jpeg')
+    number= number + 1
